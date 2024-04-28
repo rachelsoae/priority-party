9bb4707353fe25e9c20287a15372cfb65e7002e4
Rachel Prather <rachelprather1@gmail.com>
2024-04-28 16:37:07 -0700

    Create rough UI mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav>
+        <h1>Priority Party</h1>
+      </nav>
+      <div className='App__content'>
+        <main>
+          <table>
+            <tr>
+              <td></td>
+              <th>
+                Time Sensitive
+              </th>
+              <th>
+                Not Time Sensitive
+              </th>
+            </tr>
+            <tr>
+              <th>
+                Urgent
+              </th>
+              <td className='cell'>
+                <div className='zone'>
+                  <div className='card'>card1</div>
+                </div>
+              </td>
+              <td className='cell'>
+                <div className='zone'>
+                  <div className='card'>card2</div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Not Urgent
+              </th>
+              <td className='cell'>
+                <div className='zone'>
+                  <div className='card'>card3</div>
+                </div>
+              </td>
+              <td className='cell'>
+                <div className='zone'>
+                  <div className='card'>card4</div>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </main>
+        <aside>
+          <form>
+            <h2>Add a New Task</h2>
+            <input type='text'></input>
+            <button>Add New</button>
+          </form>
+          <ul>
+            <h2>My Tasks</h2>
+            {tasks.length ? tasks.map(task => <li>task.description</li>) : 'new tasks will show up here!'}
+          </ul>
+        </aside>
+        {/* SIDEBAR COMPONENT HERE */}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
